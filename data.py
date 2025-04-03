c7b10ecc01138a33fef6bbad1d759c664e62b270
Nimalan <nimalan.mahendran@gmail.com>
2025-04-03 14:27:02 -0700

    import ordering works

--- data.py
+++ data.py
@@ -1,6 +1,6 @@
 import torch
+from torch.utils.data import DataLoader, Dataset
 
-from torch.utils.data import Dataset, DataLoader
 
 class GPTDatasetV1(Dataset):
     def __init__(self, txt, tokenizer, max_length, stride):
